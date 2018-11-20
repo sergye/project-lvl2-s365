80c86bf9881425d746b5a8640f598fa3b0fd88fb
sergye <sergye@gmail.com>
2018-11-20 09:03:33 +0200

    Update dependencies and correct bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,6 @@ const program = commander;
 program
   .description('Compares two configuration files and shows a difference')
   .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .parse(process.argv);
